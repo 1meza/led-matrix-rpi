671b37ddf024003a798b6e58ec54c84f72b89f18
1meza <billyboe325@gmail.com>
2024-05-22 13:11:26 -0700

    Fix overlay maybe

--- app.py
+++ app.py
@@ -16,9 +16,19 @@ def display():
     artist = request.form['artist']
     text = f"{song_title} - {artist}"
     try:
+        # When the subprocess is run, the script will display the text on the LED matrix
+        # when a new request is made to the /display endpoint end the previous process
+        # and start a new one with the new text
+
+        if subprocess.run(["pgrep", "-f", "display_text.py"]).returncode == 0:
+            subprocess.run(["pkill", "-f", "display_text.py"], check=True)
+            logging.info("Killed previous display_text.py process")
+
         result = subprocess.run(
             ["python3", "display_text.py", text], capture_output=True, text=True, check=True
         )
+        print(result.returncode)
+        
         logging.info(f"Displayed text: {text}")
         return jsonify({'message': 'Text sent to LED Matrix!', 'status': 'success'})
     except subprocess.CalledProcessError as e:
